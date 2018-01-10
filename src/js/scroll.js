de179123fe525dcccffc5ea98c63c0f3392f7ff0
Luo <qing.luo@accenture.com>
2018-01-10 18:36:02 +0800

    Signed-off-by: Luo <qing.luo@accenture.com>

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,47 +2,73 @@ var scroll = {};
 (function (context) {
     context.init = function() {
         // context.preY  = 0;
+        context.default = {
+            "state": false,
+            "loadingcallback":function() {
+                console.log('loadingcallback');
+            },
+        };
         context.load = new IScroll('#wrapper',
-            {probeType: 3, mouseWheel: true, startY: 0, refresh: function(){
-                console.log('0000000000000000000000000000000000');
-            }});
-        context.addRefreshDom();
+            {probeType: 1, mouseWheel: true, bounce: true,
+                bounceTime: 200, preventDefault: false, startY: -50  });
+        // context.addRefreshDom();
         document.addEventListener('click', function (e) { e.preventDefault(); }, false);
 
         context.load.on("scrollStart",function(){
-            console.log("scrollStart");
+            // if(this.y === this.options.startY){
+                context.is_scrolling=true;
+                console.log("scrollStart");
+            // }
         });
 
-        context.load.on("scroll",function(){
-            console.log(this);
-            // console.log(this.preY, this.y);
-            if(this.preY > this.y){
-                // console.log('mmmmmm');
-                // console.log(this.preY, "rrrrrrrr");
-                this.preY = 0;
+        context.load.on("scroll", function(){
+            if(this.y >> 0 > 20){
+                context.default.state = true;
+                $("#refresh-dom-tip").html("释放刷新");
+                utils.rotate3d(document.querySelector("#refresh-dom .icon").style, 180);
             }else{
-                // console.log(this.preY, "ttttttttt");
-                this.preY = this.y;
+                $("#refresh-dom-tip").html("下拉加载");
+                context.default.state = false;
+                utils.rotate3d(document.querySelector("#refresh-dom .icon").style, 0);
             }
-            utils.translate(document.querySelector("#refresh-dom").style, 0, -40 + this.y);
-            // context.addRefreshDom();
-
+            console.log("scroll", this.y >> 0, context.default.state);
         });
 
-        context.load.on("scrollEnd",function(){
-            console.log("scrollEnd");
-            console.log(this.y);
-            // document.querySelector("#wrapper").style["color"] = "red";
+
+        context.load.on("scrollEnd", function(){
+            console.log("scrollEnd", this.y >> 0);
+            $("#refresh-dom").find(".pull-down").show();
+            $("#refresh-dom").find(".loading").hide();
+            if(context.is_scrolling){
+                context.is_scrolling = false;
+                if(this.y >> 0 === 0){
+                    $("#refresh-dom").find(".pull-down").hide();
+                    $("#refresh-dom").find(".loading").show();
+                    setTimeout(function() {
+                        context.load.scrollTo(0, -50, 500);
+                    }, 2000);
+                    context.default.loadingcallback();
+                }else{
+                    context.load.scrollTo(0, -50, 500);
+                }
+            }
+
             // var position = document.getElementById('wrapper');
             // utils.addClass(position,'eee');
             // var transitionType = true && {"test": "eee"};
-            // console.log(transitionType);
-            // console.log(utils.getTime());
 
-            // utils.translate(document.querySelector("#refresh-dom").style[);
+        });
 
-            context.removeDom();
-            // context.addRefreshDom();
+        context.load.on('refresh', function(){
+            // if(context.is_scrolling){
+            console.log("------");
+                if(this.y >> 0 === -50){
+                    console.log("scrollEnd------");
+                    context.default.loadcallback();
+                }else{
+                    context.load.scrollTo(0, -50, 500);
+                }
+            // }
         })
     };
 
@@ -50,7 +76,7 @@ var scroll = {};
         $("body").prepend("<div id=\"refresh-dom\" ><svg class=\"icon\" aria-hidden=\"true\" style=\"height: 20px;\">\n" +
             "                <use xlink:href=\"#icon-xialashuaxin\"></use>\n" +
             "            </svg>\n" +
-            "            <span>asfdaf</span></div>");
+            "            <span id='refresh-dom-tip'>asfdaf</span></div>");
         utils.translate(document.querySelector("#refresh-dom").style,0,-40);
         context.load.refresh();
     };
